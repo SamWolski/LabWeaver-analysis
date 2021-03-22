06776c8e9313dfc9594085c8920aa17052bf0db0
SamWolski <wlssam007@myuct.ac.za>
2021-03-23 07:45:56 +0900

    Change ConfLoader directory logic
    In order to simplify the user experience, the ConfLoader would previously append the configuration base directory automatically. While this is still desired behaviour for the end user, it was failing when the cwd was different to the desired base dir of the conf tree. After looking at the himl source, it turns out this appears to be an easy fix as leaving the `cwd` arg as None causes himl to use `os.getcwd()` and calculate the relative path from that, which is what we want. For the user, it should be easy to use `os.path.abspath()` to still use relative paths in terms of the code semantics even when an absolute path is required by the methods, as is now the case.

--- LabWeaver_analysis/ConfLoader.py
+++ LabWeaver_analysis/ConfLoader.py
@@ -10,13 +10,13 @@ class ConfLoader:
 	def __init__(self, conf_dir):
 		"""Initialize with the root directory of the configuration tree
 
-		This should be the directory that contains default.yaml (the final dir 
-		will be prepended to match the himl convention).
+		This should be the directory that contains default.yaml; the remainder of the conf path will be appended when calling the loading methods.
 		"""
-		## Peel off the final dir to prepend to himl calls
-		self.conf_folder = os.path.basename(os.path.normpath(conf_dir))
-		## Set the root configuration directory
-		self.conf_dirname = os.path.dirname(os.path.normpath(conf_dir))
+		self.conf_dir = conf_dir
+		# ## Peel off the final dir to prepend to himl calls
+		# self.conf_folder = os.path.basename(os.path.normpath(conf_dir))
+		# ## Set the root configuration directory
+		# self.conf_dirname = os.path.dirname(os.path.normpath(conf_dir))
 
 		self.himl_processor = himl.ConfigProcessor()
 		## Create attribute but leave it blank until loaded
@@ -29,9 +29,9 @@ class ConfLoader:
 		This should not include the root directory of the configuration tree 
 		(unlike when using raw himl); it will be prepended automatically.
 		"""
-		self.conf_path = self.conf_folder+"/"+conf_path
+		self.conf_path = os.path.join(self.conf_dir, conf_path)
 		self.loaded_conf = self.himl_processor.process(
-							cwd=self.conf_dirname,
+							cwd=None,
 							path=self.conf_path,
 							output_format="json",
 							**kwargs)
